f33e62f28ce4f93a462bcc8f735df8d8b8bad967
Busiwa24 <busiwaliuma@gmail.com>
2022-02-18 11:03:29 -0800

    Create migration tables

--- db/migrate/20211202100956_create_likes.rb
+++ db/migrate/20211202100956_create_likes.rb
@@ -6,3 +6,6 @@ class CreateLikes < ActiveRecord::Migration[6.1]
     end
   end
 end
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,3 @@ ActiveRecord::Schema.define(version: 2021_12_02_101431) do
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
-
-
-
