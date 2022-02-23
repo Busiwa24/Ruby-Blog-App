f5b6ca930bfcbe50ed4b3743ae98575cbbec63bc
Busiwa24 <busiwaliuma@gmail.com>
2022-02-23 13:53:31 -0800

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show]
+  root 'users#index'
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :new, :create, :show]
+    end
+  
+    resources :posts do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
-
-  resources :posts do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create]
-  end
-end
