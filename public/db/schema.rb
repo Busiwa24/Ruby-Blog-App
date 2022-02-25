2916687b8758b3983e657013376dd21a9920a3f0
Busiwa24 <busiwaliuma@gmail.com>
2022-02-25 10:33:58 +0200

    Fix linter errors

--- public/db/schema.rb
+++ public/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema
+ActiveRecord::Schema.define(version: 20_211_208_101_853)
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -58,4 +58,4 @@ ActiveRecord::Schema
   add_foreign_key 'likes', 'posts'
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
-end
+  end
