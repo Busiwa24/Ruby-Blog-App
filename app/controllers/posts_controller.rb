1009d6a449cb8caffce335010e67cd0def623c6a
Busiwa24 <busiwaliuma@gmail.com>
2022-02-25 13:24:29 +0200

    Required changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @current = current_user
+    @post = Post.new
   end
 
   def create
-    new_post = current_user.posts.build(post_params)
+    new_post = current_user.posts.new(post_params)
 
     respond_to do |format|
       format.html do
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root'users#index'
-
-  resources :users, only: [:index, :show, :new, :create] do
-    resources :posts, only: [:index, :new, :show, :create] do
-      resources :comments, only: [:new, :create]
+  root 'users#index'
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :new, :create, :show]
     end
- end
-end
- 
+  
+    resources :posts do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
+  end
